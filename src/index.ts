9e75371cbc13716e77e4194d5809f2acdcedd800
Angel Rodríguez <angelmanuelrs.com>
2019-12-20 10:59:32 -0400

    Fix: v1

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
-// @ts-ignore
 import * as simplur from 'simplur';
 import { compress } from 'compress-tag';
 
 export const z = (s: any, ...args: any) => {
-  console.log(s);
   const simplified = simplur(s, ...args);
   const compressed = compress(simplified, ...args);
-  console.log(`simplified: ${simplified}`);
-  console.log(`compressed: ${compressed}`);
+  return compressed;
 };
-
-export const run = () => {
-  z`
-    I have ${2} kitt[en|ies]\n\n
-
-    hola
-
-    {red funciono}
-  `
-}
 
